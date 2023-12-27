3048cd970e7a0b4289f84239791e7f8852cd087b
Alexander Malikov <AEMalikov@mtsbank.ru>
2023-12-27 18:09:37 +0300

    [HW-4] added test package

---
+++ src/test/java/service/SearchServiceImplTest.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+class SearchServiceImplTest {
+
+    @Test
+    void findLeapYearNames() {
+    }
+
+    @Test
+    void findOlderAnimal() {
+    }
+}
