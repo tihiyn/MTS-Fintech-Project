9d19090ed555c1c7956470628f6e0d7e483519ad
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-04-17 10:25:52 +0300

    Created new tables and relationships between them

---
+++ sql/HW-15.sql
@@ -0,0 +1,30 @@
+ALTER TABLE animals.creature ADD FOREIGN KEY (type_id) REFERENCES animals.animal_type (id);
+
+CREATE TABLE animals.habitat (
+	id int PRIMARY KEY,
+	area text
+);
+
+CREATE TABLE animals.animals_habitats (
+	id_animal_type int,
+	id_area int,
+	PRIMARY KEY (id_animal_type, id_area)
+);
+
+ALTER TABLE animals.animals_habitats ADD FOREIGN KEY (id_animal_type) REFERENCES animals.animal_type (id);
+ALTER TABLE animals.animals_habitats ADD FOREIGN KEY (id_area) REFERENCES animals.habitat (id);
+
+CREATE TABLE animals.provider (
+	id int PRIMARY KEY,
+	name text,
+	phone char(50)
+);
+
+CREATE TABLE animals.animals_providers (
+	id_animal_type int,
+	id_provider int,
+	PRIMARY KEY (id_animal_type, id_provider)
+);
+
+ALTER TABLE animals.animals_providers ADD FOREIGN KEY (id_animal_type) REFERENCES animals.animal_type (id);
+ALTER TABLE animals.animals_providers ADD FOREIGN KEY (id_provider) REFERENCES animals.provider (id);
